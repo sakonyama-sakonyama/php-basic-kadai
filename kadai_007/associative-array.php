0b05077ba4807cb53fbbc7971c3101bdc274de85
sakonyama-sakonyama <167867005+sakonyama-sakonyama@users.noreply.github.com>
2024-07-20 22:45:43 +0900

    ７章課題をアップロードしました

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>７章課題</title>
+</head>
+<body>
+  <p>
+    <?php
+    $goods_date = ['name' => 'onion', 'price' => 200, 'weight' =>160];
+    print_r($goods_date);
+    ?>
+  </p>
+  
+</body>
+</html>
